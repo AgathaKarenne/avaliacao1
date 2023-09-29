6dbbfa0290b9209d1d5d637f1ab585edbb6c4cae
AgathaKarenne <142948129+AgathaKarenne@users.noreply.github.com>
2023-09-29 13:52:00 -0300

    Update exercicio7.c

--- exercicio7.c
+++ exercicio7.c
@@ -7,42 +7,42 @@ main(){
     /*Além disso, calcule a média geral da turma. Mostre a média de cada aluno e uma mensagem "Aprovado",
     caso a média seja maior ou igual a sete, e uma mensagem "Reprovado", caso contrário. Ao final, mostre a média geral.*/
 
-    int numerosDeAlunos = 30;
-    float nota1, nota2, nota3, mediaPonderada, mediaGeral = 0.0, notas[numerosDeAlunos];
+    int i, numerosDeAlunos = 30;
+    float MP, mediaGeral = 0.0, notas[numerosDeAlunos];
 
     for (int i = 0; i < numerosDeAlunos; i++){
          float nota1, nota2, nota3;
 
         printf("Digite as notas do aluno %d:\n", i + 1);
         printf("Nota 1: ");
-        scanf("%lf", &nota1);
+        scanf("%f", &nota1);
         printf("Nota 2: ");
-        scanf("%lf", &nota2);
+        scanf("%f", &nota2);
         printf("Nota 3: ");
-        scanf("%lf", &nota3);
+        scanf("%f", &nota3);
 
-    mediaPonderada = (nota1 * 2 + nota2 * 4 + nota3 * 3) / 10;
+    MP = (nota1 * 2 + nota2 * 4 + nota3 * 3) / 10;
 
-    notas[i] = mediaPonderada;
+    notas[i] = MP;
 
-    mediaGeral += mediaPonderada;
+    mediaGeral += MP;
 
     }printf("\nMédias Ponderadas dos Alunos:\n");
     for (int i = 0; i < numerosDeAlunos; i++) {
-        printf("Aluno %d: %.2lf\n", i + 1, notas[i]);
+        printf("Aluno %d: %.2f\n", i + 1, notas[i]);
 
 
     }
-    if (mediaPonderada >= 7.0) {
-            printf("Aluno %d: %.2lf - Aprovado\n", i + 1, mediaPonderada);
+    if (MP >= 7.0) {
+            printf("Aluno %d: %.2f - Aprovado\n", i + 1, MP);
         } else {
-            printf("Aluno %d: %.2lf - Reprovado\n", i + 1, mediaPonderada);
+            printf("Aluno %d: %.2f - Reprovado\n", i - 1, MP);
         }
 
     // Calcula a média geral da turma
     mediaGeral /= numerosDeAlunos;
 
     // Exibe a média geral da turma
-    printf("\nMédia Geral da Turma: %.2lf\n", mediaGeral);
+    printf("\nMédia Geral da Turma: %.2f\n", mediaGeral);
 }
 
