272833abb87e1c22db3b91ec4f6d3b2bd2bd5d9f
AgathaKarenne <142948129+AgathaKarenne@users.noreply.github.com>
2023-11-19 16:37:35 -0300

    Update exercicio8.c

--- exercicio8.c
+++ exercicio8.c
@@ -1,8 +1,9 @@
-#include<stdio.h>
+#include <stdio.h>
 
-main(){
+main()
+{
 
-    /*A prefeitura de uma cidade fez uma pesquisa entre seus habitantes, coletando dados sobre o salário e número de filhos. A prefeitura deseja saber:  
+    /*A prefeitura de uma cidade fez uma pesquisa entre seus habitantes, coletando dados sobre o salário e número de filhos. A prefeitura deseja saber:
 
     a) média do salário da população;
 
@@ -14,12 +15,19 @@ main(){
 
     O final da leitura de dados se dará com a entrada de um salário negativo. (Use o comando do - while) .*/
 
-    int numeroDeFilhos, mediaFilhos = 0, numeroDePessoas = 0, percentualSalarioAte100;
+    int numeroDeFilhos, numeroDePessoas = 0;
+    float mediaFilhos = 0, percentualSalarioAte100;
     double salario, mediaSalario = 0, maiorSalario = 0;
-   
-    do {
+
+    do
+    {
         printf("Digite o salário:");
         scanf("%lf", &salario);
+
+        if (salario < 0)
+        {
+            break; // Saia do loop se o salário for negativo
+        }
 
         printf("Digite o número de filhos:");
         scanf("%d", &numeroDeFilhos);
@@ -28,23 +36,24 @@ main(){
         mediaFilhos += numeroDeFilhos;
         numeroDePessoas++;
 
-        if (salario > maiorSalario) {
+        if (salario > maiorSalario)
+        {
             maiorSalario = salario;
         }
 
-        if (salario <= 100.0) {
-           percentualSalarioAte100++;
+        if (salario <= 100.0)
+        {
+            percentualSalarioAte100++;
         }
-    } while (salario >= 0);
+    } while (1);
 
-    double mediaSalario = mediaSalario / numeroDePessoas;
-    double mediaFilhos = (double)mediaFilhos / numeroDePessoas;
-    double percentualSalarioAte100 = (double)percentualSalarioAte100 / numeroDePessoas * 100.0;
+    mediaSalario /= numeroDePessoas;
+    mediaFilhos /= numeroDePessoas;
+    percentualSalarioAte100 = (percentualSalarioAte100 / numeroDePessoas) * 100.0;
 
     printf("\nEstatísticas da pesquisa:\n");
     printf("a) Média do salário da população: %.2lf\n", mediaSalario);
     printf("b) Média do número de filhos: %.2lf\n", mediaFilhos);
     printf("c) Maior salário: %.2lf\n", maiorSalario);
     printf("d) Percentual de pessoas com salário até R$100,00: %.2lf%%\n", percentualSalarioAte100);
-
 }
